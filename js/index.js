639ca01e2bbb7aede73ecdea8f7bb41c5292d87c
MarceloQuiroga <j_quiroga@fpzornotza.com>
2021-10-05 11:36:13 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,9 +2,29 @@ $(document).ready(beginning);
 
 function beginning(){
   scriptMarcelo();
+  scriptIñigo();
+  scriptImrane();
 }
 
 function scriptMarcelo(){
+  $('button#login')
+    .off('click')
+    .on('click', login);
+  $('button#register')
+    .off('click')
+    .on('click', register);
+}
+
+function scriptIñigo(){
+  $('button#login')
+    .off('click')
+    .on('click', login);
+  $('button#register')
+    .off('click')
+    .on('click', register);
+}
+
+function scriptImrane(){
   $('button#login')
     .off('click')
     .on('click', login);
@@ -18,6 +38,8 @@ function preventClick(event){
   event.stopPropagation();
 }
 
+/*MARCELO SCRIPTS*/
+
 function login(event){
   event.preventClick;
   console.log(1);
@@ -28,3 +50,12 @@ function register(event){
   console.log(2);
 }
 
+/*MARCELO SCRIPTS*/
+
+/*IÑIGO SCRIPTS*/
+
+/*IÑIGO SCRIPTS*/
+
+/*IMRANE SCRIPTS*/
+
+/*IMRANE SCRIPTS*/
