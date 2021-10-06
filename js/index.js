b26f800abcb8349cdd0126ce0a92cba963921226
MarceloQuiroga <j_quiroga@fpzornotza.com>
2021-10-06 09:18:03 +0200

    Loged Funcional

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+
+/*Variables*/
+
+var JSON_Login = "";
+
+/*Variables*/
+
 $(document).ready(beginning);
 
 function beginning(){
@@ -7,6 +14,9 @@ function beginning(){
 }
 
 function scriptMarcelo(){
+
+  bbdd();
+
   $('form#login')
     .off('click')
     .on('submit', login);
@@ -40,16 +50,26 @@ function preventClick(event){
 
 /*MARCELO SCRIPTS*/
 
+function bbdd() {
+  fetch("../JSON/usuarios.json")
+    .then(function(data){return data.json();})
+    .then(function(json){JSON_Login = JSON.parse(JSON.stringify(json));});
+  
+}
+
 function login(event){
   event.preventClick;
 
-  var user = $(this).find('input#formEmail').val();
+  var status = false;
+  var email = $(this).find('input#formEmail').val();
   var password = $(this).find('input#formPassword').val();
-  
-  var JSON_Login = fetch("../JSON/usuarios.json")
-    .then(function(data){return data.json();})
-    .then(function(json){JSON_Login = json;})
-  console.log(JSON_Login);
+
+  for (var i = 0; i < JSON_Login["account"].length && status == false; i++) {
+    if (email == JSON_Login["account"][i]["email"] && password == JSON_Login["account"][i]["password"]){
+      status = true;
+      console.log("Logged");
+    }
+  };
 
   return false;
 }
