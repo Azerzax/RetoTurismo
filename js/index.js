fc50a7f4c32c2a1d51757c650f744a6a3c080984
MarceloQuiroga <j_quiroga@fpzornotza.com>
2021-10-06 13:27:50 +0200

    falta registrar

--- js/index.js
+++ js/index.js
@@ -45,7 +45,6 @@ function bbdd() {
   fetch("../JSON/usuarios.json")
     .then(function(data){return data.json();})
     .then(function(json){JSON_Login = JSON.parse(JSON.stringify(json));});
-  
 }
 
 function login(event){
@@ -61,21 +60,64 @@ function login(event){
       console.log("Logged");
       $("button#btnLogin").attr("data-bs-toggle","dropdown");
       $("button#btnLogin")[0].innerHTML = JSON_Login["account"][i]["user"];
+      $("div#login").modal("hide");
+      $("button#btnLogin").addClass("dropdown-toggle");
     }
   };
-  $("div#login").modal("hide");
-  $("button#btnLogin").addClass("dropdown-toggle");
   return false;
 }
 
 function register(event){
   event.preventClick;
-  console.log(2);
+  var username = $(this).find('input#formUser').val();
+  var email = $(this).find('input#formEmail').val();
+  var password = $(this).find('input#formPassword').val();
+  var password_x2 = $(this).find('input#formPasswordVerify').val();
+
+  var specialsChar = "!@#$%^&*()+=-[]\\\';,./{}|\":<>?";
+  var passwordVerify = true;
+  $("span.validation")[0].innerHTML = "";
+
+  if(password != password_x2) { //Diferentes Password
+    $("span.validation").removeClass("d-none");
+    $("span.validation")[0].innerHTML = "Error Contraseña Diferentes";
+  } else {
+    if (password.length < 8) { //Verifica si la contraseña es mayor a 8
+      $("span.validation").removeClass("d-none");
+      $("span.validation")[0].innerHTML = "Contraseña muy corta";
+    } else {
+      var character = false;
+      for(var i = 0; i < Array.from(password).length && character == false; i++) {
+        if(Array.from(specialsChar).indexOf(Array.from(password)[i]) != "-1"){//Verifica si contiene un caracter especial
+          passwordVerify = false;
+        }
+      }
+      for(var i = 0; i < Array.from(password).length && passwordVerify == true; i++) {
+        if(isNaN(Array.from(password)[i])){ //Verifica si contiene un numero
+          passwordVerify = false;
+        }
+      }
+  
+      if (passwordVerify == false){
+        $("span.validation").removeClass("d-none");
+        $("span.validation")[0].innerHTML = "no cumple con las politicas de seguridad";
+      } else {
+        $("button#btnLogin").attr("data-bs-toggle","dropdown");
+        $("button#btnLogin")[0].innerHTML = username;
+        $("div#login").modal("hide");
+        $("button#btnLogin").addClass("dropdown-toggle");
+      }
+    }
+  }
+  return false;
 }
 
 function logout(event) {
   event.preventClick;
-  console.log(1);
+  $("button#btnLogin")[0].innerHTML = "Login";
+  $("button#btnLogin").removeClass("dropdown-toggle");
+  $("button#btnLogin").attr("data-bs-toggle","modal");
+  $("div.dropdown-login").removeClass("show");
 }
 
 /*MARCELO SCRIPTS*/
